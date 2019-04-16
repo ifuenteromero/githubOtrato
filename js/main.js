60d0a31ea60ba2b5d1d52f4e72d5b6fd62bce763
Irene Fuente <ifuenteromero@gmail.com>
2019-04-16 02:18:02 +0200

    get user from github and get the first name

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
 'use strict';
-console.log('yeah');
+const inputEl = document.getElementById('name');
+const buttonEl = document.querySelector('.btn');
+let nameToDraw;
+const auxEl = document.querySelector('.aux');
+console.log(auxEl);
 
+function handleButtonClick(){
+    const inputValue=inputEl.value;
+    console.log(inputValue);
+    fetch('https://api.github.com/users/'+inputValue)
+    .then(response=>response.json())
+    .then(data=>{
+        let fullName = data.name;
+        return nameToDraw=getName(fullName,' ');
+    })
+    
+}
+
+   function getName(stringToDivide,sep){
+       let fullname = stringToDivide;
+       let arrayFullName = fullname.split(sep);
+       return arrayFullName[0];
+      
+
+   }
+//     fetch('https://dog.ceo/api/breeds/image/random')
+//   .then(response => response.json())
+//   .then(data => {
+//     const img = document.querySelector('img');
+//     img.src = data.message;
+//     img.alt = 'Un perro';
+//   });
+
+
+
+
+buttonEl.addEventListener('click',handleButtonClick);
